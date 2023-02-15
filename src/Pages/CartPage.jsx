ca6bffe9b6182de0a24ca9b0474c839592b8d244
Ankit <102902873+ankitdprojects@users.noreply.github.com>
2023-02-15 15:42:30 +0530

    fixing error again 1

--- src/Pages/CartPage.jsx
+++ src/Pages/CartPage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useDispatch } from "react-redux";
-import { cartActions } from "./../store/cartSlice";
+import { cartActions } from "../Store/CartSlice";
 import { Row, Col } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
 
