3bb970a00c879ffb8f6a5113cf0d700f39526b79
Ankit <102902873+ankitdprojects@users.noreply.github.com>
2023-02-15 15:49:40 +0530

    added footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='text-center' style={{background:'#FFCA2C'}}>
+      <h2>Made with ❤️ By Ankit Dhanotiya</h2>
+    </div>
   )
 }
 
